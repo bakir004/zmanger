2b0013a6c95b0f316178621133ebff603a21340c
bakir004 <bakir.cinjarevic@gmail.com>
2025-03-15 16:35:54 +0100

    Cron secret

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -1,8 +1,15 @@
 /* eslint-disable */
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest } from "next/server";
 import { logger } from "~/lib/logger";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default function handler(req: NextRequest, res: NextApiResponse) {
+  const authHeader = req.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
   logger.warn("Cron job started", { message: "Cron job started" });
   res.status(200).end("Hello Cron!");
 }
