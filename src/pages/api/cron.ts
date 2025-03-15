a6c7903a9ba0aa62582be446f9712ea7731cc3e0
bakir004 <bakir.cinjarevic@gmail.com>
2025-03-15 16:30:33 +0100

    Testing Vercels cron job

--- src/pages/api/cron.ts
+++ src/pages/api/cron.ts
@@ -3,6 +3,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { logger } from "~/lib/logger";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  logger.warn("Cron job started");
+  logger.warn("Cron job started", { message: "Cron job started" });
   res.status(200).end("Hello Cron!");
 }
