9aa48ffa9e7a7e419fb9d8ad4f9a38ed5dda2f8e
bakir004 <bakir.cinjarevic@gmail.com>
2025-05-03 15:55:50 +0200

    Updated cost

--- src/components/homepage/CostCard.tsx
+++ src/components/homepage/CostCard.tsx
@@ -7,7 +7,7 @@ export default function CostCard({ }) {
       <CardHeader className="relative">
         <DollarSign className="absolute right-6 top-6 hidden h-6 w-6 text-slate-400 lg:block" />
         <p className="text-sm">Ukupni trošak</p>
-        <CardTitle className="font-extrabold text-blue-500">$275.38</CardTitle>
+        <CardTitle className="font-extrabold text-blue-500">$295.91</CardTitle>
         <CardDescription>na servere.</CardDescription>
       </CardHeader>
     </Card>
