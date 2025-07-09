8b37fa01b042579581885cfc7dcb4813c546d68c
Bakir Cinjarevic <bakir.cinjarevic@gmail.com>
2025-07-09 14:30:20 +0200

    fresh t3 with app router

--- fonts/fonts.ts
+++
@@ -1,7 +0,0 @@
-// pages/_fonts.ts
-import { Geist, Lexend_Deca, Inter, Geist_Mono } from "next/font/google";
-
-export const geist = Geist({ subsets: ["latin"] });
-export const lexend = Lexend_Deca({ subsets: ["latin"] });
-export const geistMono = Geist_Mono({ subsets: ["latin"] });
-export const inter = Inter({ subsets: ["latin"] });
