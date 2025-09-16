c81d6841db1040694b1be45ee5c438908031646f
Bakir Cinjarevic <bakir.cinjarevic@protonmail.com>
2025-09-16 15:48:34 +0200

    fkin cors

--- next.config.js
+++ next.config.js
@@ -14,6 +14,24 @@ const config = {
 	typescript: {
 		ignoreBuildErrors: true,
 	},
+	async headers() {
+		return [
+			{
+				source: "/:path*",
+				headers: [
+					{ key: "Access-Control-Allow-Origin", value: "*" },
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET,POST,PUT,PATCH,DELETE,OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization",
+					},
+				],
+			},
+		];
+	},
 };
 
 const sentryConfig = withSentryConfig(config, {
